7ebb6a562f4a60579103d7c6146dd674719fb0c8
Marius <62169940+Marius-cpu@users.noreply.github.com>
2021-09-26 11:18:27 +0300

    Create constants(old).java

---
+++ constants(old).java
@@ -0,0 +1,20 @@
+package flib.constants;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+/* A class that is vital for fLib
+ * Contains constants already defined or just for storing purposes for later use
+ */
+public class constants {
+	public static JFrame e = new JFrame();
+	public static JPanel e2;
+	public static int i1, i2, i3, i4;
+	public static boolean raised;
+	public static int r,g,b;
+    public static int r1, r2, r3, r4;
+	public static int ov1, ov2, ov3, ov4;
+    public static int rec1,rec2,rec3,rec4,rec5,rec6;
+	public static int ar1, ar2, ar3, ar4, ar5, ar6;
+	public static int x1, y1, x2, y2;
+	}
+
