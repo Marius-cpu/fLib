6769e1780f5c675a16e19cbc802c676e7a6cd918
Marius <62169940+Marius-cpu@users.noreply.github.com>
2021-10-26 09:54:49 +0300

    Cleaned code

--- src/fLib/constants/constants.java
+++ src/fLib/constants/constants.java
@@ -7,19 +7,20 @@ import javax.sound.sampled.Clip;
 import javax.swing.JButton;                                                                                                                       
 import javax.swing.JFrame;                                                                                                                        
 import javax.swing.JPanel;                                                                                                                        
-/* A class that is vital for fLib                                                                                                                 
+/** A class that is vital for fLib                                                                                                                 
  * Contains constants already defined or just for storing purposes for later use                                                                  
  */                                                                                                                                               
-public class constants {                                                                                                                          
+public class constants
+{
 	public JButton e0 = new JButton();                                                                                                            
 	public JFrame e = new JFrame();                                                                                                               
 	public JPanel e2 = new JPanel();                                                                                                              
 	public int i1, i2, i3, i4;                                                                                                                    
 	public boolean raised;                                                                                                                        
 	public int r,g,b;                                                                                                                             
-    public int r1, r2, r3, r4;                                                                                                                    
+    	public int r1, r2, r3, r4;                                                                                                                    
 	public int ov1, ov2, ov3, ov4;                                                                                                                
-    public int rec1,rec2,rec3,rec4,rec5,rec6;                                                                                                     
+    	public int rec1,rec2,rec3,rec4,rec5,rec6;                                                                                                     
 	public int ar1, ar2, ar3, ar4, ar5, ar6;                                                                                                      
 	public int x1, y1, x2, y2, x11, x22, y11, y22;                                                                                                
 	public byte[] data;                                                                                                                           
@@ -36,6 +37,5 @@ public class constants {
 	public long currentframe;                                                                                                                     
 	public String status;                                                                                                                         
 	public AudioInputStream audioInputStream;                                                                                                     
-	public String filepath;                                                                                                                       
-	                                                                                                                                              
-	}                                     
+	public String filepath;                                                                                                                                                                                                                                                                 
+}                                     
